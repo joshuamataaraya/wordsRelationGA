eef12be6b08d3bdd97004597611dd27339c3212f
joshuamataaraya96 <joshuamata@hotmail.com>
2015-05-07 19:52:33 -0600

    proyect files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wordsRelationGaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wordsRelationGaApp
+ */
+angular.module('wordsRelationGaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
